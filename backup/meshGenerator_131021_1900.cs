2cf2c948689aaeff13ddf3be89e46d1658ffdb94
Amanda Lyvik <amanda.lyvik@gmail.com>
2021-10-14 12:34:09 +0200

    Initial commit

---
+++ backup/meshGenerator_131021_1900.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class meshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    Color[] colors;
+
+    public int xSize = 20;
+    public int zSize = 20;
+    public Gradient gradient;
+
+    float minTerrainHeight;
+
+    float maxTerrainHeight;
+
+    //public float[] amplitudes;
+    //public float[] frequencies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;  
+
+        CreateShape(); 
+    }
+
+    private void Update() {
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape() {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                // TODO: add multiple layers of noice. Check out 'octaves'
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                //float y = noiseGenerator(x, z);
+                vertices[i] = new Vector3(x, y, z);
+                
+                if (y > maxTerrainHeight)
+                {
+                    maxTerrainHeight = y;
+                }
+                if (y < minTerrainHeight)
+                {
+                    minTerrainHeight = y;
+                }
+                
+                i++;
+            }
+        }
+        
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int z = 0; z < zSize; z++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+        
+        colors = new Color[vertices.Length];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, vertices[i].y);
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
+        }
+    }
+
+    void UpdateMesh() {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos() {
+        if (vertices == null) {
+            return;
+        }
+
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(vertices[i], .1f);   
+        }
+    }
+
+    //private float noiseGenerator(int x, int z) {
+    //    float y = 0;
+    //    for (int i = 0; i < frequencies.Length; i++)
+    //    {
+    //        y += amplitudes[i] * Mathf.PerlinNoise(frequencies[i] * x, frequencies[i] * z); 
+    //    }
+    //    return y;
+    //}
+}
